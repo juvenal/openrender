3c276367b9494576f349e845b7ba41879b33bb31
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2002-10-22 12:13:06 +0000

    Completion in the cvs Id tags of some files

--- openrender/includes/sloargs.h
+++ openrender/includes/sloargs.h
@@ -18,7 +18,7 @@
 //    the Free Software Foundation; either version 2 of the License, or
 //    (at your option) any later version.
 //
-//  $Id
+//  $Id: sloargs.h,v 1.2 2002/10/22 12:13:06 juvenal Exp $
 //
 
 #ifndef SLOARGS_H
