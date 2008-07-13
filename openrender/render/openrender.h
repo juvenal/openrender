96ffadd71fa1830aec5757e2ea51d9fb3affdb17
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2008-07-13 20:14:01 +0000

    Fix on the includes of the orender build

---
+++ openrender/render/openrender.h
@@ -0,0 +1,59 @@
+//  openRender
+//
+//  openrender.h - {Summary}
+//
+//  Description:
+//    {Description}
+//
+//  Creation:
+//    Sat Mar 20 2004
+//
+//  Original Development:
+//    (C) 2004 by Juvenal A. Silva Jr. <juvenal@v2-home.com.br>
+//
+//  Contributions:
+//
+//  Statement:
+//    This program is free software, you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 2 of the License, or
+//    (at your option) any later version.
+//
+//  $Id: openrender.h,v 1.1 2008/07/13 20:14:01 juvenal.silva Exp $
+//
+ 
+#ifndef _OPENRENDER_H
+#define _OPENRENDER_H
+
+#include <string>
+
+class openRenderCLI {
+    protected:
+        static int const PKG_MAJOR_NUMBER = 0;
+        static int const PKG_MIDLE_NUMBER = 1;
+        static int const PKG_MINOR_NUMBER = 0;
+        static int const PKG_EXTRA_NUMBER = 0;
+        static char const *PKG_MSG_NAME; // = "openRender";
+        static char const *PKG_MSG_ARCH; // = "linux";
+        static char const *PKG_MSG_DESC; // = "(Adheres to the RenderMan Standard)";
+        static char const *PKG_MSG_RMAN; // = "The RenderMan (R) Interface Procedures and RIB Protocol are:\nCopyright 1988,1989, Pixar. All Rights Reserved.\nRenderMan (R) is a registered trademark of Pixar.\n";
+
+    
+    
+    
+    
+    
+};
+
+/*
+openRender 1.0 Release 1 linux (Adheres to the RenderMan Standard)
+(c) Copyright 2002 Juvenal A. Silva Jr.  Portions (c) Ian Stephenson.
+All Rights Reserved.
+ 
+The RenderMan (R) Interface Procedures and RIB Protocol are:
+Copyright 1988,1989, Pixar. All Rights Reserved.
+RenderMan (R) is a registered trademark of Pixar.
+*/
+
+
+#endif /* _OPENRENDER_H */
