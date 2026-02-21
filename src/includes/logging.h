8a587f55dee54e90d9e96e913709ec2ea034347d
Juvenal A. Silva Jr. <juvenal.silva.jr@gmail.com>
2026-02-21 10:36:21 -0300

    feat: Add logging functionality with header files for C and C++

---
+++ src/includes/logging.h
@@ -0,0 +1,66 @@
+#ifndef LOGGING_H
+#define LOGGING_H
+
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+typedef enum {
+    LOG_LEVEL_DEBUG,
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_WARN,
+    LOG_LEVEL_ERROR,
+    LOG_LEVEL_NONE
+} LogLevel;
+
+#ifdef LOGGING_IMPLEMENTATION
+    FILE* _log_output = NULL;
+    LogLevel _log_level = LOG_LEVEL_INFO;
+#else
+    extern FILE* _log_output;
+    extern LogLevel _log_level;
+#endif
+
+#define LOG_INIT(stream, level) do { \
+    _log_output = (stream); \
+    _log_level = (level); \
+} while(0)
+
+#define LOG_SET_LEVEL(level) do { \
+    _log_level = (level); \
+} while(0)
+
+#define LOG_SET_OUTPUT(stream) do { \
+    _log_output = (stream); \
+} while(0)
+
+static inline const char* _log_level_str(LogLevel level) {
+    switch(level) {
+        case LOG_LEVEL_DEBUG: return "DEBUG";
+        case LOG_LEVEL_INFO:  return "INFO ";
+        case LOG_LEVEL_WARN:  return "WARN ";
+        case LOG_LEVEL_ERROR: return "ERROR";
+        default: return "UNKWN";
+    }
+}
+
+#define LOG(level, fmt, ...) do { \
+    if ((level) >= _log_level && _log_output) { \
+        struct timespec _ts; \
+        timespec_get(&_ts, TIME_UTC); \
+        struct tm *_tm = localtime(&_ts.tv_sec); \
+        char _time_buf[24]; \
+        strftime(_time_buf, sizeof(_time_buf), "%H:%M:%S", _tm); \
+        snprintf(_time_buf + 8, sizeof(_time_buf) - 8, ".%03ld", _ts.tv_nsec / 1000000); \
+        fprintf(_log_output, "[%s] [%s] %s:%d: " fmt "\n", \
+                _time_buf, _log_level_str(level), __FILE__, __LINE__, ##__VA_ARGS__); \
+        fflush(_log_output); \
+    } \
+} while(0)
+
+#define LOG_DEBUG(fmt, ...) LOG(LOG_LEVEL_DEBUG, fmt, ##__VA_ARGS__)
+#define LOG_INFO(fmt, ...)  LOG(LOG_LEVEL_INFO,  fmt, ##__VA_ARGS__)
+#define LOG_WARN(fmt, ...)  LOG(LOG_LEVEL_WARN,  fmt, ##__VA_ARGS__)
+#define LOG_ERROR(fmt, ...) LOG(LOG_LEVEL_ERROR, fmt, ##__VA_ARGS__)
+
+#endif // LOGGING_H
---
+++ src/includes/logging.hpp
@@ -0,0 +1,79 @@
+#ifndef LOGGING_HPP
+#define LOGGING_HPP
+
+#if __cplusplus < 202002L
+    #error "logging.hpp requires C++20 or later"
+#endif
+
+#include <chrono>
+#include <ctime>
+#include <cstring>
+#include <format>
+#include <iostream>
+#include <source_location>
+#include <string_view>
+
+enum class LogLevel {
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR,
+    NONE
+};
+
+inline LogLevel      current_log_level = LogLevel::INFO;
+inline std::ostream* log_output        = &std::clog;
+
+constexpr std::string_view level_to_string(LogLevel level) {
+    switch (level) {
+        case LogLevel::DEBUG: return "DEBUG";
+        case LogLevel::INFO:  return "INFO ";
+        case LogLevel::WARN:  return "WARN ";
+        case LogLevel::ERROR: return "ERROR";
+        default:              return "UNKWN";
+    }
+}
+
+inline void set_log_level(LogLevel level)    { current_log_level = level; }
+inline void set_log_output(std::ostream& os) { log_output = &os; }
+
+struct LogMessage {
+    std::string_view     fmt;
+    std::source_location loc;
+
+    template <typename S>
+    constexpr LogMessage(const S& s,
+                         std::source_location l = std::source_location::current())
+        : fmt(s), loc(l) {}
+};
+
+template <typename... Args>
+void log(LogLevel level, LogMessage msg, Args&&... args) {
+    if (level < current_log_level || !log_output)
+        return;
+
+    const auto now   = std::chrono::system_clock::now();
+    const auto ms    = std::chrono::duration_cast<std::chrono::milliseconds>(
+                           now.time_since_epoch()) % 1000;
+    const auto now_t = std::chrono::system_clock::to_time_t(now);
+    struct tm  tm_buf{};
+    localtime_r(&now_t, &tm_buf);
+
+    char time_str[9];
+    std::strftime(time_str, sizeof(time_str), "%H:%M:%S", &tm_buf);
+
+    const std::string message = std::vformat(msg.fmt, std::make_format_args(args...));
+    *log_output << std::format("[{}.{:03d}] [{}] {}:{}: {}\n",
+                               time_str, ms.count(),
+                               level_to_string(level),
+                               msg.loc.file_name(), msg.loc.line(),
+                               message);
+    log_output->flush();
+}
+
+#define log_debug(fmt, ...) log(LogLevel::DEBUG, LogMessage{fmt}, ##__VA_ARGS__)
+#define log_info(fmt, ...)  log(LogLevel::INFO,  LogMessage{fmt}, ##__VA_ARGS__)
+#define log_warn(fmt, ...)  log(LogLevel::WARN,  LogMessage{fmt}, ##__VA_ARGS__)
+#define log_error(fmt, ...) log(LogLevel::ERROR, LogMessage{fmt}, ##__VA_ARGS__)
+
+#endif // LOGGING_HPP
